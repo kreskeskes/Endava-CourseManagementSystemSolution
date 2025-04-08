69cc5597400b16ace6c6260d8010dad399f7f4d0
ctominna <ctominna@ENDAUTOMJMUAAZT>
2025-04-08 13:24:55 +0300

    - Added functionality to seed administractor

--- CourseManagementSystem.Infrastructure/SeedIdentity/SeedIdentity.cs
+++ CourseManagementSystem.Infrastructure/SeedIdentity/SeedIdentity.cs
@@ -13,25 +13,71 @@ namespace CourseManagementSystem.Infrastructure.SeedIdentity
         private static string[] Roles = { "Admin", "Administrator", "User" };
         public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
         {
-            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
 
             if (roleManager == null)
             {
                 throw new ArgumentNullException(nameof(roleManager));
             }
-            foreach (var role in Roles )
+            foreach (var role in Roles)
             {
                 bool roleExists = await roleManager.RoleExistsAsync(role);
 
                 if (!roleExists)
                 {
-                    var results = await roleManager.CreateAsync(new IdentityRole(role));
+                    var results = await roleManager.CreateAsync(new IdentityRole<Guid>(role));
 
                     if (!results.Succeeded)
                     {
                         throw new Exception("Error while creating role.");
                     }
                 }
+            }
+
+            if (roleManager.Roles.Any())
+            {
+                await SeedAdministratorAsync(serviceProvider);
+            }
+        }
+
+        private static async Task SeedAdministratorAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser<Guid>>>();
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+
+            if (userManager == null)
+            {
+                throw new ArgumentException(nameof(userManager));
+            }
+
+            var users = userManager.Users.ToList();
+            bool administratorExists = false;
+
+            foreach (var user in users)
+            {
+                if (await userManager.IsInRoleAsync(user, "Administrator"))
+                {
+                    administratorExists = true;
+                    break;
+                }
+            }
+            if (!administratorExists)
+            {
+                var administractorUser = new IdentityUser<Guid>()
+                {
+                    Email = "administrator@example.com",
+                    UserName = "administrator@example.com"
+                };
+                string administratorPassword = "AdminPassword123!";
+
+                var result = await userManager.CreateAsync(administractorUser, administratorPassword);
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Error while seeding admin");
+                }
+
+                userManager.AddToRoleAsync(administractorUser, "Administrator");
             }
         }
     }
